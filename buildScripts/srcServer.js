3396647ea7aa8dc2642825ddbc09715bdc37d70d
Blessing Orazulume <blessing.orazulume@andela.com>
2017-10-20 23:19:36 +0100

    added mock api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -20,6 +20,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Blessing","lastName":"Oraz","email":"blessing@gmail.com"},
+    {"id": 2,"firstName":"Chidi","lastName":"Umeh","email":"chidi@yahoo.com"},
+    {"id": 3,"firstName":"Chibuzor","lastName":"Abum","email":"oyin@hotmail.com"}
+  ]);
+})
+
 app.listen(port, (err) => {
   if(err) {
     console.log(err);
