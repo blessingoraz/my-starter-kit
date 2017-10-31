b6c1e7ff0bc44ab55661eeb155d290a082849b1d
Blessing Orazulume <blessing.orazulume@andela.com>
2017-10-31 05:16:40 +0100

    bunlde splitting, bundle building for prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,32 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   plugins: [
+    //commonChunkPlugin to createa seperate bundle of vendor
+    //libraries so that they can be cached seperately
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    // Create HTML file that will reference bundledJS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    }),
     //Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
     //Minify Js
